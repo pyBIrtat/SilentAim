6292c4df1fc32686daf85daade25f14aa168ee6f
pyBIrtat <stolaevmarat@gmail.com>
2026-02-01 16:11:20 +0300

    Update Silent.lua

--- Silent.lua
+++ Silent.lua
@@ -245,7 +245,7 @@ local Window = Library:CreateWindow({Title = 'Universal Silent Aim', Center = tr
 local GeneralTab = Window:AddTab("General")
 local MainBOX = GeneralTab:AddLeftTabbox("Main") do
     local Main = MainBOX:AddTab("Main")
-    local Options = Library.Options
+    local Optioned = Library.Options
     Main:AddToggle("aim_Enabled", {Text = "Enabled"}):AddKeyPicker("aim_Enabled_KeyPicker", {Default = "RightAlt", SyncToggleState = true, Mode = "Toggle", Text = "Enabled", NoUI = false});
     Options.aim_Enabled_KeyPicker:OnClick(function()
         SilentAimSettings.Enabled = not SilentAimSettings.Enabled
@@ -470,4 +470,4 @@ oldIndex = hookmetamethod(game, "__index", newcclosure(function(self, Index)
 end))
 Main:AddLabel("Menu bind"):AddKeyPicker("MenuKeybind", { Default = "RightControl", NoUI = true, Text = "Menu keybind" })
 Main:AddButton("Unload", function() Library:Unload() end)
-Library.ToggleKeybind = Options.MenuKeybind -- Allows you to have a custom keybind for the menu
+Library.ToggleKeybind = Optioned.MenuKeybind -- Allows you to have a custom keybind for the menu
